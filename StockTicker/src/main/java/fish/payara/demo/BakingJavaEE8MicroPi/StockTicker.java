cc57d7ca517d9b93bfd3652cec148a86bec4cbb7
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2017-11-04 19:57:54 +0100

    Removed Clustered scope because buggy App failed to start after the first app was shutdown until whole Hz cluster was restarted. Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

--- StockTicker/src/main/java/fish/payara/demo/BakingJavaEE8MicroPi/StockTicker.java
+++ StockTicker/src/main/java/fish/payara/demo/BakingJavaEE8MicroPi/StockTicker.java
@@ -6,7 +6,6 @@ import javax.ejb.Startup;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
-import fish.payara.cluster.Clustered;
 import fish.payara.micro.cdi.Outbound;
 
 import java.io.Serializable;
@@ -14,7 +13,6 @@ import java.io.Serializable;
 /**
  * @author Mike Croft
  */
-@Clustered
 @Singleton
 @Startup
 public class StockTicker implements Serializable{
@@ -25,7 +23,7 @@ public class StockTicker implements Serializable{
     @Outbound(loopBack = true)
     private Event<Stock> stockEvents;
 
-    @Schedule(hour = "*", minute="*", second = "*/1", persistent = false)
+    @Schedule(hour = "*", minute="*", second = "*/1", persistent = true)
     private void generatePrice() {
         stock = new Stock("PYA", "Some very long description of Payara", Math.random() * 100.0);
         System.out.println(stock);
