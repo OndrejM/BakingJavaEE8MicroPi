f0e066a35583f57019e2aca078090262874ae7bf
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2017-11-04 19:47:10 +0100

    Workaround due to a bug with CDI bus init Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

---
+++ StockWeb/src/main/java/fish/payara/demo/BakingJavaEE8MicroPi/CdiBusInitializerWorkaround.java
@@ -0,0 +1,26 @@
+package fish.payara.demo.BakingJavaEE8MicroPi;
+
+import fish.payara.micro.cdi.ClusteredCDIEventBus;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
+/**
+ * Workaround for a bug in Payara Micro 5 Alpha 4, due to which initial CDI event isn't triggered and CDI bus isn't initialized
+ * 
+ * @author Ondrej Mihalyi
+ */
+@Singleton
+@Startup
+public class CdiBusInitializerWorkaround {
+    
+    @Inject
+    ClusteredCDIEventBus eventBus;
+    
+    @PostConstruct
+    public void startup() {
+        eventBus.initialize();
+    }
+}
+
